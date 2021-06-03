5b4f9279c88a937fedd107d14f52a0241e118f06
Niki Hansche <mf-developing@hansche.de>
2021-06-03 07:51:23 +0200

    Find better title
    Use title and author from blink-page

--- dblink.py
+++ dblink.py
@@ -2,15 +2,13 @@ from os import path
 import bs4
 import cloudscraper
 from airium import Airium
-
-PBASE_URL = 'https://www.blinkist.com'
+from urllib.parse import urlparse
 
 def scrape(url: str):
     scraper = cloudscraper.create_scraper()
     return scraper.get(url)
 
-def pbookpage(html: bs4.BeautifulSoup):
-    title:str = html.find('a', {'class':'share__facebook-icon'})['data-title']
+def pbookpage(html: bs4.BeautifulSoup, context):
     for ha1 in html.find_all('h1'):
         ha1.name='h2'
     a = Airium()
@@ -18,34 +16,40 @@ def pbookpage(html: bs4.BeautifulSoup):
     with a.html(lang="de"):
         with a.head():
             with a.title():
-                a(title)
+                a(context['title'])
             with a.style():
-                a('p {width: 80ex; line-height: 1.3; font-size: 120%;} p')
+                a('p {width: 80ex; line-height: 1.3; font-size: 120%;}')
         with a.body():
-            with a.h1():
-                a(title)
+            a(context['header'])
             for d in html('div'):
                 try:
                     if 'chapter' in d['class']:
                         a(d)
                 except KeyError:
-                    None
-    with open(path.normpath('out/'+"".join(x for x in title if x.isalnum())+'.html'),'wt') as f:
+                    pass
+    with open(path.normpath('out/'+"".join(x for x in context['title'] if x.isalnum())+'.html'),'wt') as f:
         f.write(str(a))
 
-def pstartpage(html: bs4.BeautifulSoup):
+def pstartpage(html: bs4.BeautifulSoup, context):
+    context['header'] = html.find('div', {'class':'daily-book__header'})
+    for ha3 in context['header'].find_all('h3'):
+        ha3.name = 'h1'
+    context['title'] = context['header'].find('h1').text
     for a in html('a'):
         try:
             if 'cta' in a['class']:
-                pbookpage(bs4.BeautifulSoup(scrape(PBASE_URL + a['href']).text, 'html.parser'))
+                pbookpage(bs4.BeautifulSoup(scrape(context['BaseUrl'] + a['href']).text, 'html.parser'), context)
         except KeyError:
-                None
+                pass
 
-def get_blink(url: str):
-    blink = scrape(url)
+def get_blink(bookurl: str):
+    context = {}
+    up = urlparse(bookurl)
+    context['BaseUrl'] = f'{up.scheme}://{up.netloc}'
+    blink = scrape(bookurl)
     html = bs4.BeautifulSoup(blink.text, 'html.parser')
     
-    pstartpage(html)
+    pstartpage(html, context)
 
 def main():
     get_blink('https://www.blinkist.com/de/nc/daily')
