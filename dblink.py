c452abce2c88a2df1a6da3e993cee47122ba90a1
Niki Hansche <mf-developing@hansche.de>
2021-06-05 13:32:57 +0200

    Add documentation

--- dblink.py
+++ dblink.py
@@ -9,6 +9,15 @@ from airium import Airium
 
 
 def writetofile(apath: str, filename: str, extension: str, content: str) -> None:
+    """Write content to a new file. Filename will be constructed from filename and extension.
+    If the file exists a number will be appended to filename.
+
+    Args:
+        apath (str): Filesystem path to the new file
+        filename (str): Basename for the new file
+        extension (str): Extesion of the new filename without dot
+        content (str): Content to write into the file
+    """
     if not extension.startswith('.'):
         extension = '.' + extension
     filename = "".join(x for x in filename if x.isalnum())
@@ -23,6 +32,15 @@ def writetofile(apath: str, filename: str, extension: str, content: str) -> None
 
 
 def scrape(url: str):
+    """Caching generator for cloudscraper.
+    Caches the html in `out/jar`. Uses url and todays date as the key.
+
+    Args:
+        url (str):  URL to fetch
+
+    Returns:
+        cloudscraper: An (cached) cloudscraper object
+    """
     with shelve.open('out/jar') as jar:
         key = f'{url}-{str(date.today())}'
         if key in jar:
@@ -34,6 +52,12 @@ def scrape(url: str):
 
 
 def pbookpage(html: bs4.BeautifulSoup, context):
+    """Parses the book page, generates and saves a simple html-file on `out/title.html`.
+
+    Args:
+        html (bs4.BeautifulSoup): BeautifulSoup object to parse
+        context (dict): Parsercontext with title and header keys
+    """
     for ha1 in html.find_all('h1'):
         ha1.name = 'h2'
     a = Airium()
@@ -57,6 +81,12 @@ def pbookpage(html: bs4.BeautifulSoup, context):
 
 
 def pstartpage(html: bs4.BeautifulSoup, context):
+    """Parse startpage
+
+    Args:
+        html (bs4.BeautifulSoup): BeautifulSooup object to parse
+        context (dict): Parser context
+    """
     context['header'] = html.find('div', {'class': 'daily-book__header'})
     for ha3 in context['header'].find_all('h3'):
         ha3.name = 'h1'
@@ -71,6 +101,11 @@ def pstartpage(html: bs4.BeautifulSoup, context):
 
 
 def get_blink(bookurl: str):
+    """Downloads a blink.
+
+    Args:
+        bookurl (str): Link to the startpage of the blink
+    """
     context = {}
     up = urlparse(bookurl)
     context['BaseUrl'] = f'{up.scheme}://{up.netloc}'
